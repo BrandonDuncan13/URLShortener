94e2390f86c60d8317c9ae4909145790b99a6df0
Brandon Duncan <90273289+BrandonDuncan13@users.noreply.github.com>
2024-10-07 11:15:16 -0700

    Setup Project

---
+++ components/UrlShortenerContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ShortenForm from './ShortenForm'
+import UrlList from './UrlList'
+
+export default function UrlShortenerContainer() {
+  return (
+    <div>
+      <ShortenForm />
+      <UrlList />
+    </div>
+  )
+}
