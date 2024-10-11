620324de3ce20e5d8edc4378f97dd52123fed1dd
Brandon Duncan <90273289+BrandonDuncan13@users.noreply.github.com>
2024-10-11 12:44:35 -0700

    Form to Shorten URL

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className='mx-auto max-w-xl py-12 md:py-24'>
+    // space-y-6 adds space between the children
+    <main className='mx-auto max-w-xl py-12 md:py-24 space-y-6'>
       <div className='space-y-2 text-center'>
         <h1 className='text-3xl md:text-4xl font-bold'>URL Shortener</h1>
         <p className='md:text-lg'>Shorten your URLs and share them easily</p>
--- components/ShortenForm.tsx
+++ components/ShortenForm.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+'use client';
+
+import { useState } from 'react'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
 
 export default function ShortenForm() {
+    // state for input value
+    const [url, setUrl] = useState('');
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(url);
+    }
+
   return (
-    <div>ShortenForm</div>
+    <form onSubmit={handleSubmit} className='mb-4'>
+        <div className='space-y-4'>
+            <Input
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+                className='h-12'
+                type='url'
+                placeholder='Enter URL to shorten'
+                required
+            />
+            <Button className='w-full p-2' type='submit'>
+                Shorten URL
+            </Button>
+        </div>
+    </form>
   )
 }
